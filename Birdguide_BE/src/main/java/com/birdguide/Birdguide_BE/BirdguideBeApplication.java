31f402aef250b1b927ed9caad34346e67d2b552c
Erik Moura <erikizidoro@gmail.com>
2023-12-05 20:41:33 -0300

    setup do projeto

---
+++ Birdguide_BE/src/main/java/com/birdguide/Birdguide_BE/BirdguideBeApplication.java
@@ -0,0 +1,13 @@
+package com.birdguide.Birdguide_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdguideBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdguideBeApplication.class, args);
+	}
+
+}
